c24f93965019a1f253bb5af20078b0b7a1a1a067
Jordan Irwin <antumdeluge@gmail.com>
2026-06-11 02:37:30 -0700

    Register rocky & icy asteroid nodes

---
+++ mods/nodes/init.lua
@@ -0,0 +1,12 @@
+
+core.register_node(":nodes:rock", {
+	description = "Asteroid Rock",
+	tiles = {"nodes_rock.png"},
+	diggable = false
+})
+
+core.register_node(":nodes:ice", {
+	description = "Asteroid Ice",
+	tiles = {"nodes_ice.png"},
+	diggable = false
+})
