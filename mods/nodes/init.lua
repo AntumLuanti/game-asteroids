cf0b85c9f007292f5bee6ee65d463c459afe2918
Jordan Irwin <antumdeluge@gmail.com>
2026-06-11 04:26:11 -0700

    Override mapgen to create only "space" nodes

--- mods/nodes/init.lua
+++ mods/nodes/init.lua
@@ -1,4 +1,7 @@
 
+dofile(core.get_modpath(core.get_current_modname()).."/mapgen.lua")
+
+
 core.register_node(":nodes:rock", {
 	description = "Asteroid Rock",
 	tiles = {"nodes_rock.png"},
---
+++ mods/nodes/mapgen.lua
@@ -0,0 +1,34 @@
+
+-- no air in space
+core.unregister_item("air")
+
+-- create a "space" node to simulate flying
+core.register_node(":space", {
+	description = "empty space",
+	drawtype = "airlike",
+	paramtype = "light",
+	is_ground_content = false,
+	sunlight_propagates = true,
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	climbable = true,
+	buildable_to = true,
+	floodable = true
+})
+
+
+-- override mapgen to only use space nodes
+core.set_mapgen_setting("mg_name", "singlenode", true)
+core.set_mapgen_setting("mg_flags", "nomountains, nocurves, noridges, nobiomes", true)
+core.register_on_generated(function(vmanip, minp, maxp, blockseed)
+	local vm = core.get_mapgen_object("voxelmanip")
+	local vm_data = vm:get_data()
+	local c_id = core.get_content_id("space")
+	for idx in ipairs(vm_data) do
+		vm_data[idx] = c_id
+	end
+	vm:set_data(vm_data)
+	vm:set_lighting({day=0, night=15})
+	vm:write_to_map()
+end)
