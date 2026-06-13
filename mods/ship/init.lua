ccbc65efa8293b1586bdd33f6e681539e3bea2da
Jordan Irwin <antumdeluge@gmail.com>
2026-06-13 03:24:46 -0700

    Disable sunrise

--- mods/ship/init.lua
+++ mods/ship/init.lua
@@ -3,7 +3,7 @@ core.register_on_joinplayer(function(player, last_login)
 	-- make player fly
 	player:set_physics_override({gravity=0})
 	-- disable sun, moon, & clouds
-	player:set_sun({visible=false})
+	player:set_sun({visible=false, sunrise_visible=false})
 	player:set_moon({visible=false})
 	player:set_sky({
 		type = "skybox",
