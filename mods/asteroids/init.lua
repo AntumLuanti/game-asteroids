a2dc523ff01372b482f08fd443f34798d469da56
Jordan Irwin <antumdeluge@gmail.com>
2026-06-16 20:45:11 -0700

    Register asteroids as entities instead of nodes

--- mods/asteroids/init.lua
+++ mods/asteroids/init.lua
@@ -1,13 +1,44 @@
 
+local register_asteroid = function(a_type, a_size)
+	local hp = 0
+	local size = 0
+	if a_size == "small" then
+		hp = 1
+		size = 1
+	elseif a_size == "medium" then
+		hp = 3
+		size = 2
+	elseif a_size == "large" then
+		hp = 5
+		size = 3
+	else
+		core.log("error", "unknown asteroid size: "..a_size)
+		return
+	end
 
-core.register_node(":nodes:rock", {
-	description = "Asteroid Rock",
-	tiles = {"asteroids_rock.png"},
-	diggable = false
-})
+	local collision_offset = size / 2
 
-core.register_node(":nodes:ice", {
-	description = "Asteroid Ice",
-	tiles = {"asteroids_ice.png"},
-	diggable = false
-})
+	local name = "asteroids:"..a_type.."_"..a_size
+	core.register_entity(":"..name, {
+		initial_properties = {
+			static_save = false,
+			visual = "node",
+			visual_size = {x=size, y=size, z=size},
+			textures = {"asteroids_"..a_type..".png"},
+			physical = true,
+			collide_with_objects = true,
+			collisionbox = {
+				-collision_offset, -collision_offset, -collision_offset,
+				collision_offset, collision_offset, collision_offset
+			},
+			hp_max = hp,
+			is_visible = true,
+		},
+	})
+end
+
+for _, a_type in pairs({"rock", "ice"}) do
+	for _, a_size in pairs({"small", "medium", "large"}) do
+		register_asteroid(a_type, a_size)
+	end
+end
