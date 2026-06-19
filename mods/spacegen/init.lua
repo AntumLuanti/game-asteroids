63cf48418003e456d087be08fc788aab22c08af2
Jordan Irwin <antumdeluge@gmail.com>
2026-06-19 01:20:42 -0700

    Limit viewing range to omit distance particles

--- mods/spacegen/init.lua
+++ mods/spacegen/init.lua
@@ -3,6 +3,9 @@ local time_ms = core.get_us_time() / 1000
 
 local rand = PcgRandom(time_ms)
 
+-- limit viewing range to omit distance particles
+core.settings:set("max_block_send_distance", 0)
+
 -- no air in space
 core.unregister_item("air")
 
