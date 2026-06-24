afc5499cf7d6e3bd813453b6cbe847825ae619a5
Jordan Irwin <antumdeluge@gmail.com>
2026-06-24 11:55:33 -0700

    Use enum settings to determine gameplay mode

--- mods/asteroids/api.lua
+++ mods/asteroids/api.lua
@@ -126,15 +126,20 @@ local direction_between = function(A, B)
 end
 
 
+local Mode = {
+	classic = 1,
+	endless = 2
+}
+
 -- determines if rules resembling classic gameplay should be used
-local classic_gameplay = core.settings:get_bool("asteroids.classic", false)
+local gameplay_mode = Mode[core.settings:get("asteroids.mode")] or Mode.endless
 
 --- Retrieves classic gameplay state.
 --
 --  @return
 --    `true` if classic gameplay rules are in effect.
 asteroids.is_classic_gameplay = function()
-	return classic_gameplay
+	return gameplay_mode == Mode.classic
 end
 
 
@@ -285,6 +290,7 @@ local register_asteroid = function(a_type, a_size)
 	local hp = 0
 	local size = 0
 	local point_value = 1
+	local classic_gameplay = asteroids.is_classic_gameplay()
 	if a_size == "small" then
 		hp = classic_gameplay and 1 or 3
 		size = 0.5
