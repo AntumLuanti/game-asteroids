44fc7be08975a8b32efa23505fe9989fa4811f2f
Jordan Irwin <antumdeluge@gmail.com>
2026-06-23 19:40:50 -0700

    Remove unused local variables

--- mods/asteroids/init.lua
+++ mods/asteroids/init.lua
@@ -7,10 +7,6 @@ asteroids.modpath = core.get_modpath(asteroids.modname)
 local register_asteroid = dofile(asteroids.modpath.."/api.lua")
 
 
-local rand = asteroids.rand
-local rand_thousandth = asteroids.rand_thousandth
-
-
 for _, a_type in pairs(asteroids.get_materials()) do
 	for _, a_size in pairs(asteroids.get_sizes()) do
 		register_asteroid(a_type, a_size)
