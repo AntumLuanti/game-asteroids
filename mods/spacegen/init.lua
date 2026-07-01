a0125f7355c0c9883991e889a27b59cb463e6c14
Jordan Irwin <antumdeluge@gmail.com>
2026-07-01 14:41:05 -0700

    Use global rand method

--- mods/spacegen/init.lua
+++ mods/spacegen/init.lua
@@ -1,8 +1,6 @@
 
 local time_ms = core.get_us_time() / 1000
 
-local rand = PcgRandom(time_ms)
-
 -- limit viewing range to omit distance particles
 core.settings:set("max_block_send_distance", 0)
 
@@ -17,7 +15,7 @@ core.register_on_generated(function(vmanip, minp, maxp, blockseed)
 	local vm_data = vm:get_data()
 	for idx in ipairs(vm_data) do
 		-- randomly place vacuum or particle nodes
-		local c_id = rand:next(1, 300) == 1 and core.get_content_id("vacuum_particle") or core.get_content_id("vacuum")
+		local c_id = asteroids.rand(1, 300) == 1 and core.get_content_id("vacuum_particle") or core.get_content_id("vacuum")
 		vm_data[idx] = c_id
 	end
 	vm:set_data(vm_data)
@@ -47,7 +45,7 @@ local player_ref
 local on_first_step
 on_first_step = function(dtime)
 	for _ = 1, asteroids_per_level[1], 1 do
-		local a_material = a_types[rand:next(1, #a_types)]
+		local a_material = a_types[asteroids.rand(1, #a_types)]
 		-- FIXME: not working
 		asteroids.spawn(player_ref, {material=a_material, size="large"})
 	end
@@ -84,10 +82,10 @@ core.register_globalstep(function(dtime)
 
 	local step_time = core.get_us_time() / 1000
 	if step_time - time_ms > 3000 and asteroids.can_spawn() then
-		if rand:next(1, 10) == 1 then
+		if asteroids.rand(1, 10) == 1 then
 			time_ms = step_time
-			local a_type = a_types[rand:next(1, 3)]
-			local a_size = a_sizes[rand:next(1, 3)]
+			local a_type = a_types[asteroids.rand(1, 3)]
+			local a_size = a_sizes[asteroids.rand(1, 3)]
 			local player = core.get_player_by_name("singleplayer")
 
 			local obj = asteroids.spawn(player, {material=a_type, size=a_size})
